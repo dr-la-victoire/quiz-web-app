e987890274f5d4b6a9835b9708b8198c6058d227
Victory Amadi <lavictoire633@gmail.com>
2025-02-13 20:39:56 +0100

    fix: fixed issues with state (display questions and change color for correct and incorrect answers

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -6,68 +6,60 @@ import Questions from "./questions.json";
 const Quiz = () => {
   // state to change questions
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  // state to set score
-  const [score, setScore] = useState(0);
-  // state to store user's answer
-  const [answer, setAnswer] = useState(null);
-  // state to check whether or not answer is correct
-  const [isItCorrect, setIsItCorrect] = useState(null);
+  const [userOption, setUserOption] = useState(null);
+  const [isAnswered, setIsAnswered] = useState(false);
 
-  // function to store user answer & to check correctness
-  const handleAnswer = (option) => {
-    setAnswer(option);
-    setIsItCorrect(Questions[currentQuestion].answer === option);
-
-    setInterval(() => {
-      setAnswer(null);
-      setIsItCorrect(null);
-      setCurrentQuestion((prev) => prev + 1);
-    });
-  };
-  {
-    /*
   // function to change questions as user picks an answer
-  const handleNextQuestion = () => {
-    // make sure there are still questions left in the list
-    if (currentQuestion < Questions.length - 1) {
-      setCurrentQuestion(currentQuestion + 1);
-    } else {
-      alert("Quiz completed!");
-    }
-  };*/
-  }
+  const handleNextQuestion = (option) => {
+    if (isAnswered) return;
+
+    setUserOption(option);
+    setIsAnswered(true);
+
+    // delay to show answers
+    setTimeout(() => {
+      // make sure there are still questions left in the list
+      if (currentQuestion < Questions.length - 1) {
+        setCurrentQuestion(currentQuestion + 1);
+        setUserOption(null);
+        setIsAnswered(false);
+      } else {
+        alert("Quiz completed!");
+      }
+    }, 1500);
+  };
 
   return (
-    currentQuestion < Questions.length && (
-      <div className="quiz-container">
-        <div className="question">
-          <h2>{`Question ${currentQuestion + 1}`}</h2>
-          <p>{Questions[currentQuestion].question}</p>
-        </div>
-        <div className="options">
-          {Questions[currentQuestion].options.map((option, optionIndex) => {
-            return (
-              <button
-                className={`option-button ${
-                  answer
-                    ? option === Questions[currentQuestion].answer
-                      ? "correct" // Green for correct answer
-                      : option === answer
-                        ? "wrong" // Red for wrong answer
-                        : ""
-                    : ""
-                }`}
-                key={optionIndex}
-                onClick={() => handleAnswer(option)}
-                disabled={answer !== null}
-              >
-                {option}
-              </button>
-            );
-          })}
-        </div>
+    <div className="quiz-container">
+      <div className="question">
+        <h2>{`Question ${currentQuestion + 1}`}</h2>
+        <p>{Questions[currentQuestion].question}</p>
       </div>
-    )
+      <div className="options">
+        {Questions[currentQuestion].options.map((option, optionIndex) => {
+          let className = "option-button";
+
+          if (isAnswered) {
+            if (option === Questions[currentQuestion].answer) {
+              className += "-correct";
+            } else if (option === userOption) {
+              className += "-wrong";
+            }
+          }
+
+          return (
+            <button
+              key={optionIndex}
+              className={className}
+              onClick={() => handleNextQuestion(option)}
+              disabled={isAnswered}
+            >
+              {option}
+            </button>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
