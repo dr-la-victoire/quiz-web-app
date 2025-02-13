4b56ec0626e6db12cf25574fa59facc96d9485a3
Victory Amadi <lavictoire633@gmail.com>
2025-02-13 17:30:01 +0100

    action: implemented state to make one question show at a time and choosing an answer goes to the next question

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Quiz.css";
+import Questions from "./questions.json";
 
 const Quiz = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const handleNextQuestion = () => {
+    // make sure there are still questions left in the list
+    if (currentQuestion < Questions.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      alert("Quiz completed!");
+    }
+  };
+
   return (
     <div className="quiz-container">
       <div className="question">
-        <h2>Question 1</h2>
-        <p>How many bones are in the human body</p>
+        <p>{Questions[currentQuestion].question}</p>
       </div>
       <div className="options">
-        <button className="option-button">1000</button>
-        <button className="option-button">499</button>
-        <button className="option-button">206</button>
-        <button className="option-button">58</button>
+        {Questions[currentQuestion].options.map((option, optionIndex) => {
+          return (
+            <button
+              className="option-button"
+              key={optionIndex}
+              onClick={handleNextQuestion}
+            >
+              {option}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
